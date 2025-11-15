420a6d7065c201f6823674dd58eaa3f206bcb89b
Aishwarya-14S <avhad.aishwarya1@gmail.com>
2025-11-15 22:28:58 +0530

    add login function

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from login_system import login_system
+
 for i in range(1, 500):
     i+=1
     print(i)
@@ -9,6 +11,11 @@ print("add dummy statements")
 
 while True:
     print("aaa")
+    break
 
 for j in range(1, 50):
     print(j)
+
+print("Writing endless loop for testing")
+
+login_system()
