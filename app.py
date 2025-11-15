3eb10cc4e6ebb00aeb60cd521347fdcf93e8800e
VishnuWorld123 <vrbhandari1995@gmail.com>
2025-11-15 22:04:48 +0530

    add logs

--- app.py
+++ app.py
@@ -5,21 +5,23 @@ from extensions.db import db
 
 
 def create_app():
-    print("in create app")
+    print("in create app-----------------")
     app = Flask(__name__)
     print("after defining app")
     app.config.from_object(Config)
     db.init_app(app)
     print("before table creation#############")
+    print("before table creation---------------")
     with app.app_context():
         db.create_all()
 
-    print("after table creation")
+    print("creted table")
     register_routes(app)
     print("before defining app")
     return app
 
 
 if __name__ == "__main__":
+    print("in if condition")
     app = create_app()
     app.run(debug=True)
